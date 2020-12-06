fbfc689aa768c934865cfb056481a39448203846
Silvio <silvio.dias@etec.sp.gov.br>
2020-12-06 00:55:10 -0300

    adicionar as frases motivacionais

---
+++ src/assets/Data/frases.js
@@ -0,0 +1,15 @@
+const frases = [
+  '"Siga os bons e aprenda com eles."',
+  '"O riso é a menor distância entre duas pessoa."',
+  '"Realize o óbivio, pense no improvável e conquiste o impossível."',
+  '"Acredite em milagres, mas não dependa deles."',
+  '"A maior barreira para o sucesso é o medo do fracasso."',
+  '"No meio da dificuldade encontra-se a oportunidade."',
+  '"Imagine uma nova história para sua vida e acredite nela."',
+  '"O sucesso é ir de fracasso em fracasso sem perder o entusiasmo."',
+  '"Só se pode alcançar um grande êxito quando nos mantemos fiéis a nós mesmos."',
+  '"A persistência é o caminho do êxito."',
+  '"Eu faço da dificuldade a minha motivação. A volta por cima vem na continuação."'
+]
+
+export default frases
